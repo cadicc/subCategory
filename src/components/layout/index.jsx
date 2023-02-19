cad387603b52e9f63833dd832832d7195bef9da4
Trung <88994061+cadicc@users.noreply.github.com>
2023-02-19 20:11:17 +0700

    first commit

---
+++ src/components/layout/index.jsx
@@ -0,0 +1,12 @@
+import { Grid } from "@mui/material";
+
+const Layout = ({ children }) => {
+  return (
+    <Grid>
+      <p>loz</p>
+      {children}
+    </Grid>
+  );
+};
+
+export default Layout;
